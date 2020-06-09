4587dabee7df10c48fa99a864924fea4344ab6a3
Dave Winniford <dwinniford@gmail.com>
2020-06-09 09:43:29 -0400

    saves meal plan with days

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -3,4 +3,21 @@ class MealPlansController < ApplicationController
         @meal_plans = MealPlan.all 
         render json: @meal_plans, include: [:days, :recipes, :day_recipes] 
     end
+
+    def create 
+        
+        @user = User.find_by(username: "dave")
+        @meal_plan = @user.meal_plans.create(title: params[:title])
+        @meal_plan.create_days(params[:days])
+
+        redirect_to meal_plan_path(@meal_plan)
+    end
+
+    def show 
+        @meal_plan = MealPlan.find(params[:id])
+        render json: @meal_plan, include: [:days]
+    end
+
+    private 
+    
 end
--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -3,4 +3,12 @@ class MealPlan < ApplicationRecord
     has_many :days
     has_many :recipes, through: :days
     has_many :day_recipes, through: :days
+
+    def create_days(total_days)
+        count = 1
+        while count <= total_days do 
+            day = self.days.create(number: count)
+            count += 1
+        end
+    end
 end 
