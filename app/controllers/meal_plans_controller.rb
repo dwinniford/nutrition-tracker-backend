8f78020057eb54671548beb7de1c0014f989ebce
Dave Winniford <dwinniford@gmail.com>
2020-06-09 07:16:20 -0400

    controller renders meal_plan index

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -1,2 +1,6 @@
 class MealPlansController < ApplicationController
+    def index 
+        @meal_plans = MealPlan.all 
+        render json: @meal_plans, include: [:days, :recipes, :day_recipes] 
+    end
 end
--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -2,4 +2,5 @@ class MealPlan < ApplicationRecord
     belongs_to :user
     has_many :days
     has_many :recipes, through: :days
+    has_many :day_recipes, through: :days
 end 
