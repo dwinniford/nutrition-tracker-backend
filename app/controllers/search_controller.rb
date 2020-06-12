0eb73be5a90fb4d63b5bf3234029d778ead320c3
Dave Winniford <dwinniford@gmail.com>
2020-06-12 15:57:01 -0400

    search uses credentials

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,14 +3,11 @@ require 'net/http'
 class SearchController < ApplicationController
   def recipe
     query = params[:q]
-    app_id = "836b3fdd"
-    app_key = "b8aa318b23ac94a010fea46a6e908503"
+    app_id = Rails.application.credentials.dig(:edamam, :app_id)
+    app_key = Rails.application.credentials.dig(:edamam, :app_key)
     search_url = "https://api.edamam.com/search?q=#{query}&app_id=#{app_id}&app_key=#{app_key}"
     uri = URI(search_url)
     string_response = Net::HTTP.get(uri)
-    byebug
     render json: string_response
-    # can access everything after a ?
-    # example search/recipe?q=stuff
   end
 end
