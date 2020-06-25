01c4ecdfff7f332a407ddd57eae5ee65917fc88f
Dave Winniford <dwinniford@gmail.com>
2020-06-25 10:18:39 -0400

    test SameSite: :lax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::API
     def set_csrf_cookie
         cookies["CSRF-TOKEN"] = {
             value: form_authenticity_token,
-            SameSite: :none,
+            SameSite: :lax,
             secure: true
         }
     end
