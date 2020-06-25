ce4fb068bc5dc4280baff551fca9f2610846c27c
Dave Winniford <dwinniford@gmail.com>
2020-06-25 10:13:52 -0400

    change to SameSite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::API
     def set_csrf_cookie
         cookies["CSRF-TOKEN"] = {
             value: form_authenticity_token,
-            same_site: :none,
+            SameSite: :none,
             secure: true
         }
     end
