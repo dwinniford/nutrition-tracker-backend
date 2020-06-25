38a5651d5c7ce2452d448168cc789b18ed9350af
Dave Winniford <dwinniford@gmail.com>
2020-06-25 09:42:15 -0400

    change cors site address

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080', 'https://nutrient-rich-recipes.netlify.app/'
+    origins 'http://localhost:8080', 'https://nutrient-rich-recipes.netlify.app'
 
     resource '*',
       headers: :any,
